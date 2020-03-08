44e38563e100c8d372f799aad77847325e2b1ed2
Mario Ochoa <mario8ato@hotmail.com>
2020-03-08 00:07:22 -0600

    Estructura

--- src/App.js
+++ src/App.js
@@ -1,26 +1,65 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import Title from './components/Title';
+
+
+class App extends Component {
+
+    // eslint-disable-next-line no-useless-constructor
+    // constructor(props){
+    //     super(props);
+
+    //     // this.state = {
+    //     //     numero: 0
+    //     // };
+
+    //     // this.updateNumero = this.updateNumero.bind(this)
+    // }
+
+    // updateNumero() {
+    //     setInterval(() => {
+    //         this.setState({
+    //             numero: this.state.numero + 1
+    //         })
+    //     }, 1000);
+    // }
+
+    render() {
+        return (
+            <section>
+              <div className="bg-backgrond">
+                <div style={{"width": "80%", "margin": "0 auto"}}>
+                <div className="header-main">
+                  <Title></Title>
+                  <button> Crear cuenta gratuita</button>
+                  <img className="img-illustration" src={process.env.PUBLIC_URL + '/img/placesIcon.png'} alt="Logotipo del sitio"/>
+                </div>
+                  <div>
+                    <ul>
+                      <li>
+                        <h3>Calificaciones con emociones</h3>
+                        <p>Califica los lugares con experiencias, no con numeros</p>
+                      </li>
+                      <li>
+                        <h3>¿Sin internet? Sin problemas</h3>
+                        <p>Places funciona sin internet o en conexiones lentas</p>
+                      </li>
+                      <li>
+                        <h3>Tus lugares favotiros</h3>
+                        <p>Define tus lista de sitios favoritos</p>
+                      </li>
+                    </ul>
+                  </div>
+                </div>
+              </div>
+            </section>
+        )
+    }
 }
+
+
+    
+
 
 export default App;
---
+++ src/components/Title.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+
+export default class Title extends React.Component{
+    render() {
+        return (
+            <div>
+                <h1>
+                    Places
+                </h1>
+                <p>Descubre los mejores lugares en un solo lugar</p>
+            </div>
+        )
+    }
+}
