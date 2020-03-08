41dd2eea15f76854ddafa9194e0c7e6907e02640
Mario Ochoa <mario8ato@hotmail.com>
2020-03-08 02:04:13 -0600

    Componentes material ui

--- src/App.js
+++ src/App.js
@@ -1,59 +1,96 @@
 import React, {Component} from 'react';
+import { ThemeProvider as MuiThemeProvider } from '@material-ui/core/styles';
+import Button from '@material-ui/core/Button';
+import Card from '@material-ui/core/Card';
+import CardContent from '@material-ui/core/CardContent';
+
+// Colors
+import indigo400 from '@material-ui/core/colors/indigo';
+// import blue400 from '@material-ui/core/colors/blue';
+import teal400 from '@material-ui/core/colors/teal';
+// import teal600 from '@material-ui/core/colors/teal';
+import red400 from '@material-ui/core/colors/red';
+import orange500 from '@material-ui/core/colors/orange';
+
+
+
 import './App.css';
 
 import Title from './components/Title';
 
+// variables colores
+const indigo = indigo400[400];
+// const blue = blue400[400];
+const teal = teal400[400];
+// const teal2 = teal600[600];
+const red = red400[600];
+const orange = orange500[500];
 
 class App extends Component {
 
-    // eslint-disable-next-line no-useless-constructor
-    // constructor(props){
-    //     super(props);
-
-    //     // this.state = {
-    //     //     numero: 0
-    //     // };
-
-    //     // this.updateNumero = this.updateNumero.bind(this)
-    // }
-
-    // updateNumero() {
-    //     setInterval(() => {
-    //         this.setState({
-    //             numero: this.state.numero + 1
-    //         })
-    //     }, 1000);
-    // }
 
     render() {
         return (
-            <section>
+            <MuiThemeProvider>
               <div className="bg-backgrond">
                 <div style={{"width": "80%", "margin": "0 auto"}}>
                 <div className="header-main">
                   <Title></Title>
-                  <button> Crear cuenta gratuita</button>
+
+                  <Button variant="contained" color="primary">Crear cuenta</Button>
+
+
                   <img className="img-illustration" src={process.env.PUBLIC_URL + '/img/placesIcon.png'} alt="Logotipo del sitio"/>
                 </div>
                   <div>
-                    <ul>
-                      <li>
-                        <h3>Calificaciones con emociones</h3>
-                        <p>Califica los lugares con experiencias, no con numeros</p>
-                      </li>
-                      <li>
-                        <h3>¿Sin internet? Sin problemas</h3>
-                        <p>Places funciona sin internet o en conexiones lentas</p>
-                      </li>
-                      <li>
-                        <h3>Tus lugares favotiros</h3>
-                        <p>Define tus lista de sitios favoritos</p>
-                      </li>
-                    </ul>
+                  <ul>
+                  <Card className="card-benefit">
+                      <CardContent>
+                          <div className="row">
+                              <div className="image-benefit" style={{'background-color': teal}}>
+                              <img src={process.env.PUBLIC_URL + '/img/amor.png'} alt=""/>
+                              </div>
+                              <div className="content-benefit">
+                                  <h3>Califica con emociones</h3>
+                                  <p>Califica segun tu experiencia</p>
+                              </div>
+                          </div>
+                      </CardContent>
+                  </Card>
+                  <Card className="card-benefit">
+                      <CardContent>
+                          <div className="row">
+                              <div className="image-benefit" style={{'background-color': red}}>
+                              <img src={process.env.PUBLIC_URL + '/img/sin-conexion.png'} alt=""/>
+                              </div>
+                              <div className="content-benefit">
+                                  <h3>¿Sin internet? No problem</h3>
+                                  <p>Este sitio funciona sin internet</p>
+                              </div>
+                          </div>
+                      </CardContent>
+                  </Card>
+                  <Card className="card-benefit">
+                      <CardContent>
+                          <div className="row">
+                              <div className="image-benefit" style={{'background-color': orange}}>
+                              <img src={process.env.PUBLIC_URL + '/img/estrella.png'} alt=""/>
+                              </div>
+                              <div className="content-benefit">
+                                  <h3>Tus lugares favotiros</h3>
+                                  <p>Define tus lista de sitios favoritos</p>
+                              </div>
+                          </div>
+                      </CardContent>
+                  </Card>
+                  </ul>
                   </div>
                 </div>
               </div>
-            </section>
+              <div style={{ "backgroundColor": indigo,'padding': '50px' }}>
+                    
+              </div>
+            </MuiThemeProvider>
         )
     }
 }
@@ -63,3 +100,5 @@ class App extends Component {
 
 
 export default App;
+
+//Iconos diseñados por <a href="https://www.flaticon.es/autores/freepik" title="Freepik">Freepik</a> from <a href="https://www.flaticon.es/" title="Flaticon"> www.flaticon.es</a>
